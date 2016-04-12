661e6524078e7fd9d1878c373f1cd6a7196198fa
RagT <raghutir@gmail.com>
2016-04-12 02:53:38 -0700

    Changes to Shell

--- Java/Shell.java
+++ Java/Shell.java
@@ -1,20 +1,30 @@
-import java.io.*;
-import java.util.*;
-
-public class Shell extends Thread {
-
-	//stores full command
+class Shell extends Thread
+{
+	//command line string to contain the full command
 	private String cmdLine;
 
-	//Shell default no argument constructor
-	public Shell() {
+	// constructor for shell
+	public Shell( ) {
 		cmdLine = "";
-	}	
+	}
 
-	public void run() {
+	// required run method for this Shell Thread
+	public void run( ) {
 
+		// build a simple command that invokes PingPong
+		cmdLine = "PingPong abc 100";
 
-		SysLib.cout("Done");
-		Syslib.exit();
+		// must have an array of arguments to pass to exec()
+		String[] args = SysLib.stringToArgs(cmdLine);
+		SysLib.cout("Testing PingPong\n");
+
+		// run the command
+		int tid = SysLib.exec( args );
+		SysLib.cout("Started Thread tid=" + tid + "\n");
+
+		// wait for completion then exit back to ThreadOS
+		SysLib.join();
+		SysLib.cout("Done!\n");
+		SysLib.exit();
 	}
 }
